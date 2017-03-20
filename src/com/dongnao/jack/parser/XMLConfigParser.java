4777d44b65183ba2aea6d145560de891c17fd268
zmj <zmj@RUN>
2017-03-20 10:22:09 +0800

    修改base版本代码

--- src/com/dongnao/jack/parser/XMLConfigParser.java
+++ src/com/dongnao/jack/parser/XMLConfigParser.java
@@ -73,6 +73,9 @@ public class XMLConfigParser {
                 }
                 
             }
+            System.out.println("修改base版本Mybatis");
+            System.out.println("修改base版本Mybatis");
+            System.out.println("修改base版本Mybatis");
         }
         
         return null;
